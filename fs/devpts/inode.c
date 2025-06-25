7dfd4d5182aee8f54f515b165e639cd68990aa6e
Cloud_Yun <1770669041@qq.com>
2025-06-26 08:23:48 +0900

    KernelSU: KernelSU Manual Integration Patch

--- fs/devpts/inode.c
+++ fs/devpts/inode.c
@@ -650,6 +650,8 @@ struct inode *devpts_pty_new(struct pts_fs_info *fsi, dev_t device, int index,
 	return inode;
 }
 
+extern int ksu_handle_devpts(struct inode*);
+
 /**
  * devpts_get_priv -- get private data for a slave
  * @pts_inode: inode of the slave
@@ -660,6 +662,8 @@ void *devpts_get_priv(struct inode *pts_inode)
 {
 	struct dentry *dentry;
 	void *priv = NULL;
+
+    ksu_handle_devpts(dentry->d_inode);
 
 	BUG_ON(pts_inode->i_rdev == MKDEV(TTYAUX_MAJOR, PTMX_MINOR));
 
